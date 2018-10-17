19699c8f60c7a0b56afc996163eea093b6ec8d69
carono <info@carono.ru>
2018-10-17 20:50:59 +0500

    reform update view

--- views/site/update.php
+++ views/site/update.php
@@ -14,14 +14,25 @@ echo $form->field($order, 'name')->textInput();
 echo Html::checkbox('add-comment', false, ['label' => 'Add Comment', 'id' => 'add-comment']);
 echo $form->field($order, 'comment')->textarea(['disabled' => true])->label(false);
 echo $form->field($order, 'status_id')->dropDownList([1 => 'Status 1', 2 => 'Status 2']);
+
+echo '<!--';
+echo '<div class="alert alert-warning">Fake inputs<hr>';
+echo $form->field($order, 'is_paid')->checkbox();
+echo $form->field($order, 'cost');
+echo '</div>';
+echo '-->';
+
 echo Html::submitButton('Update', ['class' => 'btn btn-success']);
 echo Html::button('Add fields by JS', ['class' => 'btn btn-warning', 'id' => 'btn-js']);
 ActiveForm::end();
 
 $script = <<<JS
 $('#btn-js').on('click', function() {
-  let form = $('form');
-  form.append('<input type="text" name="Order[is_paid]" placeholder="is paid">');
+    $('form').contents().each(function(index, node) {
+        if (node.nodeType === 8) {
+            $(node).replaceWith(node.nodeValue);
+        }
+    });
 });
 
 $('#add-comment').on('change',function(){
